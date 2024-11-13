e9b4fc693d74245ae2184a2da808711ca26aed62
Lucas <lvgvspe@icloud.com>
2024-11-13 09:31:17 -0300

    new http error

--- main.py
+++ main.py
@@ -25,6 +25,51 @@ log = create_logger("ytdownloader")
 
 root = os.path.dirname(os.path.abspath(__file__))
 
+import ssl
+from pytube import request
+from pytube import extract
+from pytube.innertube import _default_clients
+from pytube.exceptions import RegexMatchError
+
+_default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID"]
+
+import pytube, re
+def patched_get_throttling_function_name(js: str) -> str:
+    function_patterns = [
+        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
+    ]
+    for pattern in function_patterns:
+        regex = re.compile(pattern)
+        function_match = regex.search(js)
+        if function_match:
+            if len(function_match.groups()) == 1:
+                return function_match.group(1)
+            idx = function_match.group(2)
+            if idx:
+                idx = idx.strip("[]")
+                array = re.search(
+                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
+                        nfunc=re.escape(function_match.group(1))),
+                    js
+                )
+                if array:
+                    array = array.group(1).strip("[]").split(",")
+                    array = [x.strip() for x in array]
+                    return array[int(idx)]
+
+    raise RegexMatchError(
+        caller="get_throttling_function_name", pattern="multiple"
+    )
+
+ssl._create_default_https_context = ssl._create_unverified_context
+pytube.cipher.get_throttling_function_name = patched_get_throttling_function_name
 
 # Rewrite Playlist class to enable oauth within YouTube class
 class Playlist(Playlist):
@@ -265,7 +310,7 @@ def download_playlist(url):
     log.debug(f"Playlist: {url}")
     playlist = Playlist(url)
     try:
-        author = playlist.videos[0].author
+        author = playlist.videos[0].author.replace(' - Topic', '')
     except IndexError:
         log.error(f"Error: Playlist {url} is empty")
         return
